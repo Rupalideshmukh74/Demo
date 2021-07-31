a7a0bd9686d4338a3bd971c90a7c9c84ff848c31
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-07-31 23:19:05 +0530

    [Rupali] first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myfirstcsharpproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello bridgelabz!");
+            Console.ReadLine();
+        }
+       
+    }
+}
